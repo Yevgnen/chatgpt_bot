999084fd958ce2866cc7983779587325fee45767
Yevgnen Koh <wherejoystarts@gmail.com>
2023-03-09 19:24:55 +0800

    Extract complete_chat function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use std::error::Error;
 use std::sync::Arc;
 use std::{collections::HashMap, sync::Mutex};
 
@@ -27,65 +28,66 @@ async fn request_chat_completion(
     response.choices.get(0).unwrap().message.content.clone()
 }
 
+async fn complete_chat(
+    bot: Bot,
+    client: Client,
+    chat_histories: Arc<Mutex<ChatHistories>>,
+    msg: Message,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let content = msg.text().unwrap();
+    log::info!(
+        "Receive message user: {}, content: {}",
+        msg.chat.id,
+        content
+    );
+
+    let hists;
+    {
+        let mut guard = chat_histories.lock().unwrap();
+        let messages = guard.entry(msg.chat.id).or_default();
+        messages.push(
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::User)
+                .content(content)
+                .build()
+                .unwrap(),
+        );
+        hists = messages.clone();
+    }
+
+    let response = request_chat_completion(&client, hists, None).await;
+
+    {
+        let mut guard = chat_histories.lock().unwrap();
+        let messages = guard.entry(msg.chat.id).or_default();
+        messages.push(
+            ChatCompletionRequestMessageArgs::default()
+                .role(Role::Assistant)
+                .content(response.clone())
+                .build()
+                .unwrap(),
+        );
+    }
+
+    bot.send_message(msg.chat.id, response).await?;
+
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() {
     pretty_env_logger::init();
 
     let bot = Bot::from_env();
-    let client = Client::new();
 
+    let client = Client::new();
     let chat_histories = Arc::new(Mutex::new(ChatHistories::new()));
 
-    let handler = Update::filter_message().endpoint(
-        move |bot: Bot, chat_histories: Arc<Mutex<ChatHistories>>, msg: Message| {
-            let client = client.clone();
-
-            let content = msg.text().unwrap();
-            log::info!(
-                "Receive message user: {}, content: {}",
-                msg.chat.id,
-                content
-            );
-
-            let hists;
-            {
-                let mut guard = chat_histories.lock().unwrap();
-                let messages = guard.entry(msg.chat.id).or_default();
-                messages.push(
-                    ChatCompletionRequestMessageArgs::default()
-                        .role(Role::User)
-                        .content(content)
-                        .build()
-                        .unwrap(),
-                );
-                hists = messages.clone();
-            }
-
-            async move {
-                let response = request_chat_completion(&client, hists, None).await;
-
-                {
-                    let mut guard = chat_histories.lock().unwrap();
-                    let messages = guard.entry(msg.chat.id).or_default();
-                    messages.push(
-                        ChatCompletionRequestMessageArgs::default()
-                            .role(Role::Assistant)
-                            .content(response.clone())
-                            .build()
-                            .unwrap(),
-                    );
-                }
-
-                bot.send_message(msg.chat.id, response).await?;
-
-                respond(())
-            }
-        },
-    );
+    let handler = Update::filter_message().endpoint(complete_chat);
 
     Dispatcher::builder(bot, handler)
         // Pass the shared state to the handler as a dependency.
-        .dependencies(dptree::deps![chat_histories])
+        .dependencies(dptree::deps![client, chat_histories])
         .enable_ctrlc_handler()
         .build()
         .dispatch()
